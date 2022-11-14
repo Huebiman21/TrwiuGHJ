df4a9f38eba9e93b8802fe850f759e15910beddf
Huebiman21 <104358762+Huebiman21@users.noreply.github.com>
2022-11-14 20:38:13 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,9 @@
 """Latexify root package."""
-
-try:
     from latexify import _version
 
     __version__ = _version.__version__
 except:
     __version__ = ""
-
-from latexify import frontend
 
 get_latex = frontend.get_latex
 
